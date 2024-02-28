ef59ffa7dd0e772d41b6e971b1e262f1cdd144af
Mujtaba Mubasher <mujtabamubasher11@gmail.com>
2024-02-29 02:33:38 +0500

    Done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema, } from "mongoose";
 import bcrypt from 'bcrypt'
-import { Jwt } from "jsonwebtoken";
+import  jwt  from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -67,7 +67,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
 };
 
 userSchema.methods.generateAccessToken = function () {
-  return jwt.sing(
+  return jwt.sign(
     {
       _id: this._id,
       email: this.email,
@@ -82,7 +82,7 @@ userSchema.methods.generateAccessToken = function () {
 };
 
 userSchema.methods.generateRefreshToken = function () {
-  return jwt.sing(
+  return jwt.sign(
     {
       _id: this._id,
     },
